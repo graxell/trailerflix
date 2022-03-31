ea5c0f145ba0521ceb63ad03a9c2aadb6a5d19d0
graxell <45760779+graxell@users.noreply.github.com>
2022-03-31 20:50:21 +0100

    Update test2.js

--- src/test2.js
+++ src/test2.js
@@ -10,7 +10,6 @@ import logo from "./assets/images/logo.png";
 import "./App.css";
 
 function App() {
-  const API_KEY = "api_key=887d05f637b9e4864f8cec83c7da0a1f";
   const ENDPOINT_URL = "https://api.themoviedb.org/3/";
   const DEFAULT_PARAM =
     "&language=en-US&sort_by=popularity.desc&include_video=false&page=1";
@@ -332,7 +331,6 @@ import "./App.css";
 import NewAndPopular from "./pages/NewAndPopular";
 
 function App() {
-  const API_KEY = "api_key=887d05f637b9e4864f8cec83c7da0a1f";
   const ENDPOINT_URL = "https://api.themoviedb.org/3/";
   const DEFAULT_PARAM =
     "&language=en-US&sort_by=popularity.desc&include_video=false&page=1";
